369a3bbf360856377e8725933077d43536fcec27
phuong <phuong.ta011290@gmail.com>
2024-08-13 10:33:19 +0300

    routers created!

--- app.py
+++ app.py
@@ -8,5 +8,30 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/languages')
+def hello_languages():  # put application's code here
+    return 'Hello Languages!'
+
+
+@app.route('/skills')
+def hello_skills():  # put application's code here
+    return 'Hello Skills!'
+
+
+@app.route('/experience')
+def hello_experience():  # put application's code here
+    return 'Hello Experience!'
+
+
+@app.route('/education')
+def hello_educations():  # put application's code here
+    return 'Hello Education!'
+
+
+@app.route('/hobbies')
+def hello_hobbies():  # put application's code here
+    return 'Hello Hobbies!'
+
+
 if __name__ == '__main__':
     app.run()
