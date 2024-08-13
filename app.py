9e091a34889d92ad3ba03e08613ebab9f5a18a81
phuong <phuong.ta011290@gmail.com>
2024-08-13 14:05:57 +0300

    templates created!

--- app.py
+++ app.py
@@ -1,36 +1,52 @@
 from flask import Flask
+from markupsafe import escape
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template('about_me.html')
 
 
 @app.route('/languages')
-def hello_languages():  # put application's code here
-    return 'Hello Languages!'
+def list_languages():  # put application's code here
+    return render_template('languages.html')
 
 
-@app.route('/skills')
-def hello_skills():  # put application's code here
-    return 'Hello Skills!'
-
-
-@app.route('/experience')
-def hello_experience():  # put application's code here
-    return 'Hello Experience!'
+@app.route('/experiences')
+def list_experiences():  # put application's code here
+    return render_template('experiences.html')
 
 
 @app.route('/education')
-def hello_educations():  # put application's code here
-    return 'Hello Education!'
+def list_education():  # put application's code here
+    return render_template('education.html')
 
 
-@app.route('/hobbies')
-def hello_hobbies():  # put application's code here
-    return 'Hello Hobbies!'
+@app.route('/skills')
+def list_skills():  # put application's code here
+    project = "hello Phuong"
+    return render_template('skills.html', project=project)
+
+
+@app.route('/interests')
+def list_interests():  # put application's code here
+    project = "hello Phuong"
+    return render_template('interests.html', project=project)
+
+
+@app.route('/projects')
+def list_projects():
+    # Greet the user
+    return render_template('projects.html')
+
+
+@app.route('/projects/<project>')
+def show_project(project):
+    # Greet the user
+    return render_template('project.html', project=project)
 
 
 if __name__ == '__main__':
